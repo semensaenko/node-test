7377c41919abb20fc1dfe83015996c44f1508dd1
semensaenko <Saenko.Senya@gmail.com>
2016-09-24 02:09:49 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# node-test
-Token based authentication
+Test project description:
+* Project should consist of 2 parts: back-end (API) and front-end.
+* You should use mongodb database (mongoose package or any other)
+* Make it possible to perform all user actions via the API, including authentication.
+* Code quality is very important and project structure should be well organized.
+
+Required functionality:
+* User must be able to create an account and log in
+* User can add (and edit and delete) a time record with 3 fields: note about what he has worked on, date, time spent
+* User should be able to see how much time he worked each day on "Report" page, this page should have date filter from-to. Example report:
+
+  Date  | Total time | Notes
+  ----  | ---------- | -----
+  21.07 | 9h         | "Login&registration", "Another note"
+
+How to work on this project:
+
+1. Fork this repository (you will need a github account)
+2. Write code. Try to make meaningful commits.
+3. When test project is complete push code to github and send us a link to your(forked) repository
---
+++ app.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var path = require('path');
+var assert = require('assert');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var config = require('./config/config');
+
+
+
+var app = express();
+
+app.locals.moment = require('moment');
+
+mongoose.connect(config.mongodb.uri);
+require('./model/models')(app, mongoose);
+
+
+app.use(function(req, res, next){
+   res.locals.user = '';
+   res.locals.users = '';
+   res.locals.title = '';
+   res.locals.message = '';
+   res.locals.success = '';
+  
+   next();
+});
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+
+
+
+// view engine setup
+app.config = config;
+app.set('superSecret', config.secret);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'client')));
+var routes = require('./routes/index')(app);;
+//app.use('/', routes);
+//app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
